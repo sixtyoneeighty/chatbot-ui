3bb511c7d20638dc517f21dc0613910fad7cbf0d
Jacob Ambroz <143131645+sixtyoneeighty@users.noreply.github.com>
2025-02-08 17:38:25 -0600

    c

--- lib/generate-local-embedding.ts
+++ lib/generate-local-embedding.ts
@@ -1,17 +1,3 @@
-import { pipeline } from "@xenova/transformers"
-
-export async function generateLocalEmbedding(content: string) {
-  const generateEmbedding = await pipeline(
-    "feature-extraction",
-    "Xenova/all-MiniLM-L6-v2"
-  )
-
-  const output = await generateEmbedding(content, {
-    pooling: "mean",
-    normalize: true
-  })
-
-  const embedding = Array.from(output.data)
-
-  return embedding
+export async function generateLocalEmbedding(text: string) {
+  throw new Error("Local embeddings are not supported")
 }
--- next.config.js
+++ next.config.js
@@ -3,30 +3,29 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 })
 
 const withPWA = require("next-pwa")({
-  dest: "public"
+  dest: "public",
+  disable: process.env.NODE_ENV === "development"
 })
 
-module.exports = withBundleAnalyzer(
-  withPWA({
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "localhost"
-        },
-        {
-          protocol: "http",
-          hostname: "127.0.0.1"
-        },
-        {
-          protocol: "https",
-          hostname: "**"
-        }
-      ]
-    },
-    experimental: {
-      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
-    }
-  })
-)
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost"
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1"
+      },
+      {
+        protocol: "https",
+        hostname: "**"
+      }
+    ]
+  }
+}
+
+module.exports = withBundleAnalyzer(withPWA(nextConfig))
